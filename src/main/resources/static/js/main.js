152f229a74b34c208dc72b0fe51a90220d085c7c
fengyifeng <fengyifeng1104@126.com>
2017-01-06 18:16:52 +0800

    test

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,32 +6,16 @@
 		else
 		return null;
 		}
-
-
-$(function() {
-
-	
-	//userid  注册
-	window.localStorage.user = JSON.stringify({uid: 3383});
-	var user = JSON.parse(localStorage.user) || {uid: 0};
-	$('#curUserId').html(user.uid);
-	
-	
-	//current tab 设置默认值
-	window.localStorage.currentTab = localStorage.currentTab || 0;
-
-	$('#myTabs > li > a').on('click', function(ev) {
-		var index = $(this).parent().index();
-		localStorage.currentTab = index;
-	})
-	
-	
-	//点击tab响应事件
-	function selectCase(ev) {
+//点击tab响应事件
+	function selectCase(ev,pageNum) {
 		ev.preventDefault();
 		//Url to do 
-		$.get("/caseManage/getCaseList?page=1&size=3",function(data){
+		pageNum = pageNum ||  1;
+		$.get("/caseManage/getCaseList?page="+pageNum+"&size=2",function(data){
 			console.log('catalog data: ', data);
+			
+			localStorage.page=JSON.stringify({"totalPage":data.totalPages});
+			
 			$("#case_table tbody").empty()
 			var $tbody = $('#case_table tbody');
 			var htmlStr = '';
@@ -67,6 +51,12 @@ $(function() {
 			
 	    });
 	    $(ev.target).tab('show');	
+	    
+	    	//分页
+		var element = $('#case_page');//获得数据装配的位置
+	     var pageN = JSON.parse(localStorage.page).totalPage;
+	     element.bootstrapPaginator(pageInit(pageN));	//进行初始化
+    
 	}
 	
 	function selectTemplate(ev) {
@@ -140,8 +130,72 @@ $(function() {
 		   $(ev.target).tab('show');	
 	}
 	
+	function pageInit(totalPage){
+
+		var nP = 5
+		if (totalPage){
+			nP = totalPage;
+		}
+		//初始化所需数据
+       var options = {
+       		showNext:false,
+       		next:false,
+       		showPrevious:false,
+       		prev:false,
+            bootstrapMajorVersion:3,//版本号。3代表的是第三版本
+            currentPage: 1, //当前页数
+            numberOfPages: nP, //显示页码数标个数
+            totalPages:totalPage, //总共的数据所需要的总页数
+            itemTexts: function (type, page, current) {  
+            		//图标的更改显示可以在这里修改。
+            switch (type) {  
+                    case "first":  
+                        return "首页";  
+                    case "last":  
+                        return "末页";  
+                    case "page":  
+                        return  page;  
+                }                 
+            }, 
+            tooltipTitles: function (type, page, current) {
+				//如果想要去掉页码数字上面的预览功能，则在此操作。例如：可以直接return。
+                switch (type) {
+		            case "first":
+		                return "Go to first page";
+		            case "last":
+		                return "Go to last page";
+		            case "page":
+		                return (page === current) ? "Current page is " + page : "Go to page " + page;
+		        }
+            },
+            onPageClicked: function (e, originalEvent, type, page) {
+            	$('.pagination li > a:empty').hide();  
+                 selectCase(e,page);
+            }
+        };
+        return options;
+	}
+$(function() {
+
+	
+	//userid  注册
+	window.localStorage.user = JSON.stringify({uid: 3383});
+	var user = JSON.parse(localStorage.user) || {uid: 0};
+	$('#curUserId').html(user.uid);
+	
+	
+	//current tab 设置默认值
+	window.localStorage.currentTab = localStorage.currentTab || 0;
+
+	$('#myTabs > li > a').on('click', function(ev) {
+		var index = $(this).parent().index();
+		localStorage.currentTab = index;
+	})
+	
+	
+	
 	$('#caseMge').on('click', selectCase);
-	$('#caseRefresh').on('click',selectCase);
+	$('#caseRefresh').on('click', selectCase);
 	
 	$('#templateMge').on('click', selectTemplate);
 	$('#templateRefresh').on('click', selectTemplate);
@@ -157,6 +211,11 @@ $(function() {
 	$(document).on('click', '[data-ref=detail]', function(ev) {
 		localStorage.shouldGet = String(1);
 	})
+	
+	
+	
+
 
 	 
 })
+
