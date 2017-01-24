c3d6a80f91bd2d14e72f27df08d7ca88853e1eb7
fengyifeng <fengyifeng1104@126.com>
2017-01-24 10:51:42 +0800

    update

--- src/main/resources/static/js/addscene.js
+++ src/main/resources/static/js/addscene.js
@@ -11,8 +11,8 @@
 		console.log("ff",repo);
 		console.log("gg",repo.mark);
       //if (repo.loading) return repo.text;
-			
-    
+
+
 		//var markup ='<span>'+repo.templateName+'</span>';
 		var markup ="<div class='select2-result-repository__title'>模板Id: " + repo.templateId + " - 模板名称:["+repo.templateName+"]</div>";
       	return markup;
@@ -22,13 +22,13 @@
 		$('#select2-templateId-container').attr('data-id',repo.templateId)
       return repo.templateId;
     }
-    
-    
-    
+
+
+
 $(function() {
 	var user = JSON.parse(localStorage.user) || {uid: 0};
 	$('#curUserId').html(user.uid);
-	
+
 	$(".js-data-example-ajax").select2({
 	  ajax: {
 	 type: 'GET',
@@ -41,7 +41,7 @@ $(function() {
 			templateName:params.term, // search term
 		  };
 		},
-			
+
 
 		processResults: function (data, params) {
 			console.log("content",data);
@@ -60,37 +60,37 @@ $(function() {
 	templateResult: formatRepo, // omitted for brevity, see the source of this page
 	templateSelection: formatRepoSelection
 	});
-	
-	
-	
+
+
+
 
 	$('.J_menus li > a').on('click', function(ev) {
 		$('#reqMethod').text($(this).text());
 	})
-	
+
 	$('#save_addscene').on('click', function(ev) {
 			ev.preventDefault();
-			
+
 		 	var v_templateId = $('#select2-templateId-container').attr('data-id');
 		 	var v_sceneDescription = $('#sceneDescription').val();
-		 	
+
 		 	if (v_templateId==undefined || v_sceneDescription==""){
 		 		alert("保存失败, 模板Id or 场景描述 不能为空")
 		 	}else{
-		 	
+
 			 	var v_reqMethod = $('#reqMethod').text();
 			 	var v_sceneHost = $('#sceneHost').val();
 			 	var v_sceneApi = $('#sceneApi').val();
 			 	var v_modelDescription = $('#modelDescription').val();
 			 	var v_mark = $('#mark').val();
-	
-			 	
-		
+
+
+
 				 $.ajax({
 				 		type: 'POST',
 				 		contentType: "application/json; charset=utf-8",
 				 		url: '/sceneManage/addScene',
-				 	
+
 				 		data: JSON.stringify({"userId":user.uid,
 				 							"sceneDescription":v_sceneDescription,
 				 							"reqMethod":v_reqMethod,
@@ -99,7 +99,7 @@ $(function() {
 				 							"modelDescription":v_modelDescription,
 				 							"templateId":v_templateId,
 				 							"mark":v_mark
-				 		
+
 				 							}),
 				 		dataType : 'json'
 				 	}).done(function(data) {
@@ -109,13 +109,13 @@ $(function() {
 				 		alert("保存失败,服务端返回失败")
 				 });
 			 }
-		 		
-		 	
-		 
-		 
+
+
+
+
 		return false;
 	})
-	
-	
-	
+
+
+
 })
